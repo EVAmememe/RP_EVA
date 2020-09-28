e7d05397a24c010d2a2d66bdb2985c50fcd39058
EVAmememe <70974878+EVAmememe@users.noreply.github.com>
2020-09-28 19:32:53 +0400

    rpg2

--- Js/main.js
+++ Js/main.js
@@ -1,92 +1,144 @@
 var rooms = [
 [ 
-    'коридор 0',
+    'коридор0',
     'Перед тобой дверь в столовую и дверь в аудиторию. Куда пойдёшь?',
     ['налево','да','аудиторию','направо','встоловую','нет','наулицу','выйти'],
     [2,2,2,1,1,4,6,6],
-    null
+    0,
+    "kartinki/room0.jpg"
 ],[
-    'столовка 1',
+    'столовка1',
     'Ты обожрался. Хочешь ещё поесть, или пойдёшь учиться?',
     ['да','нет','хочуучиться'],
     [1,1,0],
-    null
+    1,
+    "kartinki/room1.jpg"
 ],[
-    'аудитория 2',
+    'аудитория2',
     'Ты в аудитории.Хочешь заниматься или пойдешь в туалет?',
     ['да','заниматься','хочуучиться','туалет','обратно','нет','нехочу','люблюучиться'],
     [2,5,5,3,0,0,4,5],
-    null
+    2,
+    "kartinki/room2.jpg"
 ],[
-    'туалет 3',
+    'туалет3',
     'Ты сходил в туалет. Пойдёшь на пары или домой?',
     ['да','заниматься','хочуучиться','напары','останусь','вкоридор','нет','нехочу','домой'],
     [2,2,2,2,3,0,4,4,4],
-    null
+    3,
+    "kartinki/room3.jpg"
 ],[
-    'военкомат 4',
+    'военкомат4',
     'Ты в военкомате. Тебя уже не спасти! ',
     [],
     [],
-    null
+    4,
+    "kartinki/room4.jpg"
 ],[
-    'Дом 5',
+    'Дом5',
     'Пары кончились. Ты молодец!',
     [],
     [],
-    null
+    5,
+    "kartinki/room5.jpg"
 ],[
-    'Улица 6',
+    'Улица6',
     'Ты на улице. Куда пойдёшь?',
     ['курить','обратно','да','нет','домой'],
     [7,0,0,0,4],
-    null
+    6,
+    "kartinki/room6.jpg"
 ],[
-    'Сигарета 7',
+    'Сигарета7',
     'КУРЕНИЕ УБИВАЕТ!',
-    [],
-    [],
-    null
+    ['курить','обратно','да','нет','домой','заниматься','хочуучиться','напары'],
+    [1,6,6,6,6,6,6,6],
+    6,
+    "kartinki/room7.jpg"
 ]
 
 ];
 
 var step = 0;
-while (1) {
+document.getElementById('location').innerHTML = rooms[0][0];
+document.getElementById('result').innerHTML = rooms[0][1];
+var hp = 100;
+var money = 100;
+
+function stepTo() {
+    var answer = document.getElementById('inp').value;
+    document.getElementById('inp').value = '';
+    answer = answer.toLowerCase().replace(' ','');
     if (step === null) {
-        break;
+        return;
     }
     var room = rooms[step];
     if (!room) {
-        break;
+        return;
     }
-    var answer = prompt(room[1]);
     if (!answer) {
-        continue;
+        return;
     }
-    answer = answer.toLowerCase().replace(' ','');
-    if (step === 4 || step === 7 ) {
-        alert ('GAME OVER');
-        break;
+    if (step === 4 || hp<=0 ) {
+        document.getElementById("imageGO").classList.remove("imagePassiv");
+        //var elem = document.getElementById("imageGO");
+        //elem.classList.remove("imagePassiv");
+        return;
     }
     if (step === 5 ) {
-        alert ('YOU ARE WINNER');
-        break;
+        document.getElementById("imageYW").classList.remove("imagePassiv");
+        return;
     }
     var isWayNotFound = true;
     for (var i=0;i < room[2].length;i++) {
         if (answer === room[2][i]) {
+            hp -= 5;
             step = room [3][i];
             isWayNotFound = false;
             break;
         }
     }
     if (isWayNotFound ) {
-        console.log('неправильно')
-    //    step = room[4];
+        console.log('неправильно');
+        step = room[4];
+        return; 
     }
+    if (step === 1 && money >= 20 && hp < 100) {
+        money -= 20;
+        hp = 100;
+    } else if (step === 7) {
+        hp -= 25;
+    }
+    printRoomInfo();
 }
-
+function printRoomInfo(){
+    if (step != null && rooms[step]){
+        document.getElementById('location').innerHTML = "Локация: " + rooms[step][0];
+    }
+    var b = '';
+    var room = rooms[step];
+    var exits = [];
+    for(var i = 0; i < room[3].length; i++) {
+        var isNameUniq = true;
+        var num = room[3][i];
+        for(var j = 0; j < exits.length; j++) {
+            if(rooms[num][0] == exits[j]) {
+                isNameUniq = false;
+            }
+        }
+        if(isNameUniq){
+            exits.push(rooms[num][0]); 
+        }        
+    }
+    document.getElementById('ways').innerHTML = exits.join(', ');
+    document.getElementById('image').src = rooms[step][5];
+    document.getElementById('money').innerHTML = 'Деньги студента: ' + money + ' рублей';
+    document.getElementById('hp').innerHTML = 'Здоровье студента: ' + hp + '%';
+    document.getElementById('result').innerHTML = rooms[step][1];
+}
+var btn = document.getElementById('step');
+btn.addEventListener('click', stepTo);
+printRoomInfo();
     /*switch (step) {
         case 0: 
         answer = prompt('Перед тобой дверь в столовую и дверь в аудиторию. Куда пойдёшь?');
